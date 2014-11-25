35a7409676c0e95d4bb8378ed2f0e194b1dc5356
Allan Burns <allanlburns@yahoo.com>
2014-11-24 23:25:21 -0500

    added MySQL test route to routes.php.

--- app/routes.php
+++ app/routes.php
@@ -17,7 +17,7 @@ Route::get('/', function()
 	return View::make('index');
 });
 
-// test route for environment function from lecture 8 notes:
+// test route for environment function from lecture 8 notes/environments:
 
 Route::get('/get-environment',function() {
 
@@ -25,12 +25,27 @@ Route::get('/get-environment',function() {
 
 });
 
-//Trigger-error route as per lecture 8 notes:
+//Trigger-error route as per lecture 8 notes/environments:
 
 Route::get('/trigger-error',function() {
 
     # Class Foobar should not exist, so this should create an error
     $foo = new Foobar;
+
+});
+
+//MySQL test route from lecture 8 notes/database configuration:
+
+Route::get('mysql-test', function() {
+
+    # Print environment
+    echo 'Environment: '.App::environment().'<br>';
+
+    # Use the DB component to select all the databases
+    $results = DB::select('SHOW DATABASES;');
+
+    # If the "Pre" package is not installed, you should output using print_r instead
+    echo Pre::render($results);
 
 });
 
