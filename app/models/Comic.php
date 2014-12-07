5db5412c609881bff6a8845638aae232442dbfbb
Allan Burns <allanlburns@yahoo.com>
2014-12-06 19:07:04 -0500

    Defined relationship between stores and comics in Store and Comic models.

--- app/models/Comic.php
+++ app/models/Comic.php
@@ -1,6 +1,13 @@
 <?php
 
 class comic extends Eloquent {
+
+	public function store() {
+		#Comic belongs to store
+		# One-to-many relationship between stores and comics:
+		return $this->belongsTo('Store');
+
+	}
 	
 
 }
--- app/models/Store.php
+++ app/models/Store.php
@@ -2,6 +2,12 @@
 
 class Store extends Eloquent {
 
+	public function comic() {
+		#Store has many comics
+		# One-to-many relationship between stores and comics:
+		return $this->hasMany('Comic');
+
+	}
 }
 
 ?>
