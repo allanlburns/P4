278b3b5c296b22805835236d273a94482485f51b
Allan Burns <allanlburns@yahoo.com>
2014-12-08 17:46:44 -0500

    Added log-in/log-out/signup links to _master.blade.php.

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>@yield('title', 'In-Store Subscription Service')</title>
+	<title>@yield('title', 'Online Comic Subscription Interface!')</title>
 </head>
 <body>
+	<h1>Welcome to the Comic Online Subscription Interface!</h1>
+	<ul>
+		@if(Auth::check())
+			<li><a href='/logout'>Log out {{ Auth::user()->email; }}</a></li>
+			
+		@else
+			<li><a href='/signup'>Sign up</a> or <a href='/login'>Log in</a></li>
+		@endif
+		</ul>
 	@yield('content')
 </body>
 </html>
--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,6 +1,6 @@
 @extends('_master')
 @section('content')
-<h1>In-Store Subscription Service</h1>
+<h2>Home</h2>
 <p>Here you can create and edit your own comic book pull list for Wednesday morning! Never miss an issue! Add and drop as you go!
 
 <p>
