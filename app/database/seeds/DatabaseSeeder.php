f411d108e1137dc0352dee42a0705fbc3fadd0bf
Allan Burns <allanlburns@yahoo.com>
2014-12-15 00:02:09 -0500

    Added Marvel comics to database seeder.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -107,7 +107,325 @@ class DatabaseSeeder extends Seeder {
 		$harvard->store_name = 'Harvard Square';
 		$harvard->save();
 
-		
+		$avengers = new Comic;
+		$avengers->title = 'Avengers';
+		$avengers->publisher = 'Marvel';
+		$avengers->save();
+
+		$avengersai = new Comic;
+		$avengersai->title = 'Avengers A.I.';
+		$avengersai->publisher = 'Marvel';
+		$avengersai->save();
+
+		$avengersundercover = new Comic;
+		$avengersundercover->title = 'Avengers Undercover';
+		$avengersundercover->publisher = 'Marvel';
+		$avengersundercover->save();
+
+		$avengersworld = new Comic;
+		$avengersworld->title = 'Avengers World';
+		$avengersworld->publisher = 'Marvel';
+		$avengersworld->save();
+
+		$blackwidow = new Comic;
+		$blackwidow->title = 'Black Widow';
+		$blackwidow->publisher = 'Marvel';
+		$blackwidow->save();
+
+		$captainamerica = new Comic;
+		$captainamerica->title = 'Captain America';
+		$captainamerica->publisher = 'Marvel';
+		$captainamerica->save();
+
+		$captainmarvel = new Comic;
+		$captainmarvel->title = 'Captain Marvel';
+		$captainmarvel->publisher = 'Marvel';
+		$captainmarvel->save();
+
+		$daredevil = new Comic;
+		$daredevil->title = 'Daredevil';
+		$daredevil->publisher = 'Marvel';
+		$daredevil->save();
+
+		$deadlyhands = new Comic;
+		$deadlyhands->title = 'Deadly Hands of Kung-Fu';
+		$deadlyhands->publisher = 'Marvel';
+		$deadlyhands->save();
+
+		$deadpool = new Comic;
+		$deadpool->title = 'Deadpool';
+		$deadpool->publisher = 'Marvel';
+		$deadpool->save();
+
+		$dexter = new Comic;
+		$dexter->title = 'Dexter';
+		$dexter->publisher = 'Marvel';
+		$dexter->save();
+
+		$elektra = new Comic;
+		$elektra->title = 'Elektra';
+		$elektra->publisher = 'Marvel';
+		$elektra->save();
+
+		$empire = new Comic;
+		$empire->title = 'Empire of the Dead';
+		$empire->publisher = 'Marvel';
+		$empire->save();
+
+		$fantasticfour = new Comic;
+		$fantasticfour->title = 'Fantastic Four';
+		$fantasticfour->publisher = 'Marvel';
+		$fantasticfour->save();
+
+		$fearlessdefenders = new Comic;
+		$fearlessdefenders->title = 'Fearless Defenders';
+		$fearlessdefenders->publisher = 'Marvel';
+		$fearlessdefenders->save();
+
+		$figment = new Comic;
+		$figment->title = 'Figment';
+		$figment->publisher = 'Marvel';
+		$figment->save();
+
+		$ghostrider = new Comic;
+		$ghostrider->title = 'Ghost Rider';
+		$ghostrider->publisher = 'Marvel';
+		$ghostrider->save();
+
+		$gotg = new Comic;
+		$gotg->title = 'Guardians of the Galaxy';
+		$gotg->publisher = 'Marvel';
+		$gotg->save();
+
+		$hawkeye = new Comic;
+		$hawkeye->title = 'Hawkeye';
+		$hawkeye->publisher = 'Marvel';
+		$hawkeye->save();
+
+		$hulk = new Comic;
+		$hulk->title = 'Hulk';
+		$hulk->publisher = 'Marvel';
+		$hulk->save();
+
+		$inhuman = new Comic;
+		$inhuman->title = 'Inhuman';
+		$inhuman->publisher = 'Marvel';
+		$inhuman->save();
+
+		$ironfist = new Comic;
+		$ironfist->title = 'Iron Fist';
+		$ironfist->publisher = 'Marvel';
+		$ironfist->save();
+
+		$ironman = new Comic;
+		$ironman->title = 'Superior Iron Man';
+		$ironman->publisher = 'Marvel';
+		$ironman->save();
+
+		$ironpatriot = new Comic;
+		$ironpatriot->title = 'Iron Patriot';
+		$ironpatriot->publisher = 'Marvel';
+		$ironpatriot->save();
+
+		$invaders = new Comic;
+		$invaders->title = 'All-New Invaders';
+		$invaders->publisher = 'Marvel';
+		$invaders->save();
+
+		$loki = new Comic;
+		$loki->title = 'Loki: Agent of Asgard';
+		$loki->publisher = 'Marvel';
+		$loki->save();
+
+		$mightyavengers = new Comic;
+		$mightyavengers->title = 'Might Avengers';
+		$mightyavengers->publisher = 'Marvel';
+		$mightyavengers->save();
+
+		$miracleman = new Comic;
+		$miracleman->title = 'Miracleman';
+		$miracleman->publisher = 'Marvel';
+		$miracleman->save();
+
+		$moonknight = new Comic;
+		$moonknight->title = 'Moon Knight';
+		$moonknight->publisher = 'Marvel';
+		$moonknight->save();
+
+		$msmarvel = new Comic;
+		$msmarvel->title = 'Ms. Marvel';
+		$msmarvel->publisher = 'Marvel';
+		$msmarvel->save();
+
+		$newavengers = new Comic;
+		$newavengers->title = 'New Avengers';
+		$newavengers->publisher = 'Marvel';
+		$newavengers->save();
+
+		$newwarriors = new Comic;
+		$newwarriors->title = 'New Warriors';
+		$newwarriors->publisher = 'Marvel';
+		$newwarriors->save();
+
+		$nova = new Comic;
+		$nova->title = 'Nova';
+		$nova->publisher = 'Marvel';
+		$nova->save();
+
+		$originalsin = new Comic;
+		$originalsin->title = 'Original Sin';
+		$originalsin->publisher = 'Marvel';
+		$originalsin->save();
+
+		$painkillerjane = new Comic;
+		$painkillerjane->title = 'Painkiller Jane';
+		$painkillerjane->publisher = 'Marvel';
+		$painkillerjane->save();
+
+		$punisher = new Comic;
+		$punisher->title = 'Punisher';
+		$punisher->publisher = 'Marvel';
+		$punisher->save();
+
+		$savagehulk = new Comic;
+		$savagehulk->title = 'Savage Hulk';
+		$savagehulk->publisher = 'Marvel';
+		$savagehulk->save();
+
+		$secretavengers = new Comic;
+		$secretavengers->title = 'Secret Avengers';
+		$secretavengers->publisher = 'Marvel';
+		$secretavengers->save();
+
+		$shehulk = new Comic;
+		$shehulk->title = 'She-Hulk';
+		$shehulk->publisher = 'Marvel';
+		$shehulk->save();
+
+		$silversurfer = new Comic;
+		$silversurfer->title = 'Silver Surfer';
+		$silversurfer->publisher = 'Marvel';
+		$silversurfer->save();
+
+		$thor = new Comic;
+		$thor->title = 'Thor';
+		$thor->publisher = 'Marvel';
+		$thor->save();
+
+		$thunderbolts = new Comic;
+		$thunderbolts->title = 'Thunderbolts';
+		$thunderbolts->publisher = 'Marvel';
+		$thunderbolts->save();
+
+		$whatif = new Comic;
+		$whatif->title = 'What If?';
+		$whatif->publisher = 'Marvel';
+		$whatif->save();
+
+		$uncannyavengers = new Comic;
+		$uncannyavengers->title = 'Uncanny Avengers';
+		$uncannyavengers->publisher = 'Marvel';
+		$uncannyavengers->save();
+
+		$youngavengers = new Comic;
+		$youngavengers->title = 'Young Avengers';
+		$youngavengers->publisher = 'Marvel';
+		$youngavengers->save();
+
+		$asm = new Comic;
+		$asm->title = 'Amazing Spider-Man';
+		$asm->publisher = 'Marvel';
+		$asm->save();
+
+		$spidey2099 = new Comic;
+		$spidey2099->title = 'Spider-Man 2099';
+		$spidey2099->publisher = 'Marvel';
+		$spidey2099->save();
+
+		$muavengers = new Comic;
+		$muavengers->title = 'Marvel Universe: Avengers Assemble';
+		$muavengers->publisher = 'Marvel';
+		$muavengers->save();
+
+		$muspidey = new Comic;
+		$muspidey->title = 'Marvel Universe: Spider-Man';
+		$muspidey->publisher = 'Marvel';
+		$muspidey->save();
+
+		$oz = new Comic;
+		$oz->title = 'Oz Comics';
+		$oz->publisher = 'Marvel';
+		$oz->save();
+
+		$miles = new Comic;
+		$miles->title = 'Miles Morales: Ultimate Spider-Man';
+		$miles->publisher = 'Marvel';
+		$miles->save();
+
+		$ultimates = new Comic;
+		$ultimates->title = 'All-New Ultimates';
+		$ultimates->publisher = 'Marvel';
+		$ultimates->save();
+
+		$powers = new Comic;
+		$powers->title = 'Powers';
+		$powers->publisher = 'Marvel/Icon';
+		$powers->save();
+
+		$usom = new Comic;
+		$usom->title = 'United States of Murder';
+		$usom->publisher = 'Marvel/Icon';
+		$usom->save();
+
+		$mph = new Comic;
+		$mph->title = 'MPH';
+		$mph->publisher = 'Marvel/Icon';
+		$mph->save();
+
+		$allnewxmen = new Comic;
+		$allnewxmen->title = 'All-New X-Men';
+		$allnewxmen->publisher = 'Marvel';
+		$allnewxmen->save();
+
+		$amazingxmen = new Comic;
+		$amazingxmen->title = 'Amazing X-Men';
+		$amazingxmen->publisher = 'Marvel';
+		$amazingxmen->save();
+
+		$cyclops = new Comic;
+		$cyclops->title = 'Cyclops';
+		$cyclops->publisher = 'Marvel';
+		$cyclops->save();
+
+		$magneto = new Comic;
+		$magneto->title = 'Magneto';
+		$magneto->publisher = 'Marvel';
+		$magneto->save();
+
+		$nightcrawler = new Comic;
+		$nightcrawler->title = 'Nightcrawler';
+		$nightcrawler->publisher = 'Marvel';
+		$nightcrawler->save();
+
+		$xfactor = new Comic;
+		$xfactor->title = 'All-New X-Factor';
+		$xfactor->publisher = 'Marvel';
+		$xfactor->save();
+
+		$xforce = new Comic;
+		$xforce->title = 'X-Force';
+		$xforce->publisher = 'Marvel';
+		$xforce->save();
+
+		$uncanny = new Comic;
+		$uncanny->title = 'Uncanny X-Men';
+		$uncanny->publisher = 'Marvel';
+		$uncanny->save();
+
+		$xmen = new Comic;
+		$xmen->title = 'X-Men';
+		$xmen->publisher = 'Marvel';
+		$xmen->save();
 
 
 	}
