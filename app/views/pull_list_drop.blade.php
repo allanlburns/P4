4dac9e118d00be4fe6340137c7dbcb4552607735
Allan Burns <allanlburns@yahoo.com>
2014-12-16 15:59:19 -0500

    Added foreach loop title listings to pull-list/drop view.

--- app/views/pull_list_drop.blade.php
+++ app/views/pull_list_drop.blade.php
@@ -4,13 +4,13 @@ View Your Pull List
 @stop
 @section('content')
 
-<h1>Drop a comic</h1>
+<h1>Drop a comic from your pull list here!</h1>
 
-
-Code to drop a comic:
 
 {{ Form::open(array('url' => '/pull-list/drop')) }}
-
+	
+	<h3>enter the comic id # to the right of the title you wish to drop and hit "submit query".</h3>
+	
 	<p>Comic:</p>
 	{{ Form::text('comic_id') }}
 
@@ -18,8 +18,43 @@ Code to drop a comic:
 
 {{ Form::close() }}
 
+<br><br>
 
-<p>
+<h3>Marvel</h3>
+<?php
+
+	$comics = DB::table('comics')->where('publisher', 'LIKE', '%Marvel%')->get(); 
+
+	foreach($comics as $comic) {
+            echo $comic->title . " " . "(id =" . $comic->id. ")".  '<br><br>';
+    }
+?>
+
+<h3>DC</h3><br>
+
+
+<h3>Image</h3>
+
+<?php
+
+	$comics = DB::table('comics')->where('publisher', 'LIKE', '%Image%')->get(); 
+
+	foreach($comics as $comic) {
+            echo $comic->title . " " . "(id =" . $comic->id. ")".  '<br><br>';
+    }
+?>
+
+<h3>IDW</h3>
+<?php
+
+	$comics = DB::table('comics')->where('publisher', 'LIKE', '%IDW%')->get(); 
+
+	foreach($comics as $comic) {
+            echo $comic->title . " " . "(id =" . $comic->id. ")".  '<br><br>';
+    }
+?>
+
+
 
 <br><br>
 
