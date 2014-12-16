6eea270b125f984a94c04014ae885d5fd99702c1
Allan Burns <allanlburns@yahoo.com>
2014-12-16 14:42:57 -0500

    Added foreach loop with publisher constraint hard coded into pull-list/add view and gave explanation.

--- app/views/pull_list_add.blade.php
+++ app/views/pull_list_add.blade.php
@@ -8,13 +8,24 @@ Add a Comic to Your Pull List
 
 
 {{ Form::open(array('url' => '/pull-list/add')) }}
-
+	
+	<h3>enter the comic id # to the right of the title you wish to and and hit "submit query".</h3>
+	
 	<p>Comic:</p>
 	{{ Form::text('comic_id') }}
 
 	{{ Form::submit() }} 
 
 {{ Form::close() }}
+
+<?php
+
+	$comics = DB::table('comics')->where('publisher', 'LIKE', '%Image%')->get(); 
+
+	foreach($comics as $comic) {
+            echo $comic->title . " " . "(id =" . $comic->id. ")".  '<br><br>';
+    }
+?>
 
 
 <p>
--- app/views/pull_list_view.blade.php
+++ app/views/pull_list_view.blade.php
@@ -10,14 +10,7 @@ Click <a href='/pull-list/add'>here</a> to add a comic to your list. <br><br>
 
 Click <a href='/pull-list/drop'>here</a> to drop a comic from your list. <br><br>
 
-<?php
 
-	$comics = DB::table('comics')->get();
-
-	foreach($comics as $comic) {
-            echo $comic->title . $comic->id.'<br>';
-    }
-?>
 
 <br><br>
 
